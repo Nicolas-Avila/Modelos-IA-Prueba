b732afc8c2bf16952df7af434638fc192b3b2825
Nicolas <aviladanielnicolas@gmail.com>
2025-05-30 17:15:28 -0300

    que ropa usar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import OpenAI from "openai";
 import readline from "readline";
-
+import { weather } from "./apiClima.js";
 dotenv.config();
 
 const openai = new OpenAI({
@@ -23,20 +23,27 @@ function askQuestion(query) {
     });
 }
 
+
 let conversationHistory = [];
 
-async function chat() {
-    while (true) {
-        const question = await askQuestion('Que me queres preguntar? ');
+async function clothingTemperature() {
+    const temperatura = await weather();
+    console.log(temperatura)
+    //while (true) {
+        // const question = await askQuestion('Que me queres preguntar? ');
 
-        if (question.trim().toLowerCase() === 'cerrar') {
-            console.log('Chau!');
-            rl.close();
-            break;
-        }
+        // if (question.trim().toLowerCase() === 'cerrar') {
+        //     console.log('Chau!');
+        //     rl.close();
+        //     break;
+        // }
 
         // Agregar la pregunta del usuario al historial
-        conversationHistory.push({ role: "user", content: question });
+        conversationHistory.push({
+            role: "user",
+            content: `${temperatura} quiero que me digas de manera amigable que ropa me recomendas usar`
+          });
+          
 
         try {
             const chat = await openai.chat.completions.create({
@@ -53,55 +60,57 @@ async function chat() {
         } catch (error) {
             console.error('esto no funco', error);
         }
+    //}
+}
+
+async function img() {
+    while (true) {
+        const link = await askQuestion('Ingrese link de la imagen: ');
+        const question = await askQuestion('Que me queres preguntar? ');
+
+        if (question.trim().toLowerCase() === 'cerrar') {
+            console.log('Chau!');
+            rl.close();
+            break;
+        }
+
+        // Agregar la pregunta del usuario al historial
+        conversationHistory.push({
+            role: "user", content: [
+                {
+                    "type": "text",
+                    "text": question
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": link
+                    }
+                }
+            ]
+        });
+
+        try {
+            const chat = await openai.chat.completions.create({
+                model: 'meta-llama/llama-4-maverick:free',
+                messages: conversationHistory
+            });
+
+            const answer = chat.choices[0].message;
+            console.log('\nMi respuesta:', answer, '\n');
+            console.log('------------------------------------------------------------------ \n \n \n')
+
+            conversationHistory.push({ role: "assistant", content: answer });
+
+        } catch (error) {
+            console.error('esto no funco', error);
+        }
     }
 }
 
 
-chat();
-// async function img() {
-//     while (true) {
-//         const link = await askQuestion('Ingrese link de la imagen: ');
-//         const question = await askQuestion('Que me queres preguntar? ');
 
-//         if (question.trim().toLowerCase() === 'cerrar') {
-//             console.log('Chau!');
-//             rl.close();
-//             break;
-//         }
-
-//         // Agregar la pregunta del usuario al historial
-//         conversationHistory.push({
-//             role: "user", content: [
-//                 {
-//                     "type": "text",
-//                     "text": question
-//                 },
-//                 {
-//                     "type": "image_url",
-//                     "image_url": {
-//                         "url": link
-//                     }
-//                 }
-//             ]
-//         });
-
-//         try {
-//             const chat = await openai.chat.completions.create({
-//                 model: 'meta-llama/llama-4-maverick:free',
-//                 messages: conversationHistory
-//             });
-
-//             const answer = chat.choices[0].message;
-//             console.log('\nMi respuesta:', answer, '\n');
-//             console.log('------------------------------------------------------------------ \n \n \n')
-
-//             conversationHistory.push({ role: "assistant", content: answer });
-
-//         } catch (error) {
-//             console.error('esto no funco', error);
-//         }
-//     }
-// }
 
 
 // img();
+clothingTemperature();
